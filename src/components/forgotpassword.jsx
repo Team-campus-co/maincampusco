6a6efc85aa1841366d5673a71773b97cf1b8c130
Mona Lisa <sohamsolat@gmail.com>
2025-09-05 00:14:39 +0530

    deploy

--- src/components/forgotpassword.jsx
+++ src/components/forgotpassword.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import ImageWithBasePath from "../../../core/common/imageWithBasePath";
+// import ImageWithBasePath from "../../../core/common/imageWithBasePath";
 
 const ForgotPassword = () => {
   
