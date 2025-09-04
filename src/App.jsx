5e6ab0f2e0c0c3e6fa2d9aa3004b5d1fedffa80a
Mona Lisa <sohamsolat@gmail.com>
2025-09-05 00:22:50 +0530

    dev

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,15 @@
-import { useState } from 'react'
-import auth_campuscommander_logo from './assets/react.svg'
-import auth_campuscommander_logo1 from '/vite.svg'
 import './App.css'
-import ForgotPassword from './components/forgotpassword'
+import ForgotPassword from "./components/forgotpassword";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-    <div>
-      <h1 style={{ textAlign: "center" }}>Welcome to CampusCo 🚀</h1>
       <ForgotPassword />
-    </div>
-      <div>
-        
-       
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> Save DATA TO CAMPUS
-        </p>
-      </div>
-      <p className="read-the-docs">
-       Welcome to Campus Commander Administartion
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
+
+ 
+
